e77751d35608488c4a70b478d23e00451aec181b
skukreja <skukreja434@gmail.com>
2020-03-24 02:53:27 +0530

    'upload-files-integrated'

--- models/projects.js
+++ models/projects.js
@@ -1,51 +1,50 @@
-const mongoose = require('mongoose');
-const Joi = require('@hapi/joi');
-Joi.objectId = require('joi-objectid')(Joi);
+const mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
+Joi.objectId = require("joi-objectid")(Joi);
 
 const projectSchema = new mongoose.Schema({
-    // user_id: mongoose.Types.ObjectId,
-    approve: Boolean,
-    author: {
-        author_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'user'    
-        },
-        author_name: String
+  // user_id: mongoose.Types.ObjectId,
+  approve: Boolean,
+  author: {
+    author_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user"
     },
-    category: String,
-    content: String,
-    date: Date,
-    heading: String,
-    // id: String,
-    image: String,
-    subHeading: String,
-    link: String
+    author_name: String
+  },
+  category: String,
+  content: String,
+  date: Date,
+  heading: String,
+  // id: String,
+  image: String,
+  subHeading: String,
+  link: String
 });
 
-const Project = mongoose.model('Project',projectSchema);
+const Project = mongoose.model("Project", projectSchema);
 
+const validateProject = data => {
+  const schema = Joi.object({
+    // author: string,
+    author: Joi.object({
+      author_id: Joi.objectId(),
+      author_name: Joi.string()
+    }),
+    link: Joi.string(),
+    approve: Joi.boolean(),
+    category: Joi.string(),
+    content: Joi.string(),
+    date: Joi.date(),
+    heading: Joi.string(),
+    // blog_id: Joi.objectId(),
+    image: Joi.string(),
+    subheading: Joi.string()
+    // email: Joi.string()
+  });
 
-const validateProject = (data) => {
-    const schema = Joi.object( {
-        // author: string,
-        author: Joi.object({
-            author_id: Joi.objectId(),
-            author_name: Joi.string(),
-        }),
-        link : Joi.string(),
-        approve: Joi.boolean(),
-        category: Joi.string(),
-        content: Joi.string(),
-        date: Joi.date(),
-        heading: Joi.string(),
-        // blog_id: Joi.objectId(),
-        image: Joi.string(),
-        subheading: Joi.string(),
-        // email: Joi.string()
-    });
-
-    return schema.validate(data);
-}
+  return schema.validate(data);
+};
 
 module.exports.Project = Project;
 module.exports.validateProject = validateProject;
